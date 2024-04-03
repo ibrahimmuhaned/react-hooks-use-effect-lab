57509e70f4d84ad6dc8b0e2c3d3f92b8ae127235
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-04-03 03:36:41 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,46 @@
+// import React, { useState } from "react";
+// import Question from "./Question";
+// import quiz from "../data/quiz";
+
+// function App() {
+//   const [questions, setQuestions] = useState(quiz);
+//   const [currentQuestionId, setCurrentQuestion] = useState(1);
+//   const [score, setScore] = useState(0);
+//   const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+
+//   function handleQuestionAnswered(correct) {
+//     if (currentQuestionId < questions.length) {
+//       setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
+//     } else {
+//       setCurrentQuestion(null);
+//     }
+//     if (correct) {
+//       setScore((score) => score + 1);
+//     }
+//   }
+
+//   return (
+//     <main>
+//       <section>
+//         {currentQuestion ? (
+//           <Question
+//             question={currentQuestion}
+//             onAnswered={handleQuestionAnswered}
+//           />
+//         ) : (
+//           <>
+//             <h1>Game Over</h1>
+//             <h2>Total Correct: {score}</h2>
+//           </>
+//         )}
+//       </section>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
 import React, { useState } from "react";
 import Question from "./Question";
 import quiz from "../data/quiz";
@@ -19,7 +62,19 @@ function App() {
     }
   }
 
-  return (
+
+  function handleQuestionAnswered(correct) {
+    if (currentQuestionId < questions.length) {
+      setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
+    } else {
+      setCurrentQuestion(null);
+    }
+    if (correct) {
+      setScore((score) => score + 1);
+    }
+  }
+
+   return (
     <main>
       <section>
         {currentQuestion ? (
@@ -39,3 +94,4 @@ function App() {
 }
 
 export default App;
+
